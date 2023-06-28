05e8fb80d661ada961c8b2ec19f410004e0b1a5d
Marcus Kim <marcuskim1989@gmail.com>
2023-06-27 21:39:48 -0700

    Problem 1314. Need help from Braden.

---
+++ LeetCodeSolved.playground/Sources/1314. Matrix Block Sum .swift
@@ -0,0 +1,28 @@
+import Foundation
+
+class Solution {
+    func matrixBlockSum(_ mat: [[Int]], _ k: Int) -> [[Int]] {
+        
+        /*
+            create a matrix answer by exploring a submatrix
+
+            [
+                [1, 2, 3],
+                [4, 5, 6],
+                [7, 8, 9]
+            ]
+
+            starting with [0][0] in mat, we look at a submatrix localized around the number 1
+
+            since k is 1. We only look at 1 element in each direction
+
+            1 + 2 + 4 + 5 = 12
+
+            12 is then inserted into [0][0] of the answer matrix
+
+        */
+
+        
+
+    }
+}
