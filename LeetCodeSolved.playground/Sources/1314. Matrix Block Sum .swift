8b6d8bc10279129703da2f4a5c4fd30844c13595
Marcus Kim <marcuskim1989@gmail.com>
2023-06-29 20:51:10 -0700

    Problem 1314. Was able to solved. First, loop through initial matrix with nested for loops. Within the nested loops, establish low and high ranges for both the x and y axes. Create two additional nested for loops, that iterate through the established ranges. Within these inner-most loops, track a growing sum variable. When ranges are exhausted, insert sum into the corresponding place in the result matrix

--- LeetCodeSolved.playground/Sources/1314. Matrix Block Sum .swift
+++ LeetCodeSolved.playground/Sources/1314. Matrix Block Sum .swift
@@ -1,6 +1,5 @@
 import Foundation
-
-class Solution {
+class Solution1314 {
     func matrixBlockSum(_ mat: [[Int]], _ k: Int) -> [[Int]] {
         
         /*
@@ -22,7 +21,82 @@ class Solution {
 
         */
 
+         /*
+                    x1(low): i - k unless this is less than zero, in which case just use zero
+                    x2(high): i + k unless this is more than mat.count, in which case just use mat.count
+                    y1(low): j - k unless this is less than zero, in which case just use zero
+                    y2(low): j + k unless this more than mat[i].count, which case just use mat[i].count
+
+                    let x1 = Math.max(i-k, 0)
+                    let x2 = Math.min(i+k, mat.count)
+
+                    let sum = 0;
+
+                */
+
+        var result = mat
+
         
+        for i in 0..<mat.count {
+            for j in 0..<mat[i].count {
+
+                var xLow: Int {
+                    if i - k < 0 {
+                        return 0
+                    } else {
+                        return i - k
+                    }
+                }
+
+                var xHigh: Int {
+                    if i + k > mat.count - 1{
+                        return mat.count - 1
+                    } else {
+                        return i + k
+                    }
+                }
+
+                var yLow: Int {
+                    if j - k < 0 {
+                        return 0
+                    } else {
+                        return j - k
+                    }
+                }
+
+                var yHigh: Int {
+                    if j + k > mat[j].count - 1{
+                        return mat[j].count - 1
+                    } else {
+                        return j + k
+                    }
+                }
+
+                print(xLow, xHigh, yLow, yHigh)
+                // for x in x1..x2 {
+                    //   for y in y1..y2 {
+                    //       sum = sum + mat[x][y]
+                    //   }
+                    // }
+
+                    // result[i][j] = sum
+
+                    var sum = 0
+
+                    for x in xLow...xHigh {
+                        for y in yLow...yHigh{
+                            sum += mat[x][y]
+                        }
+                    }
+
+                result[i][j] = sum
+
+            }
+        }
+
+
+        return result
+
 
     }
 }
