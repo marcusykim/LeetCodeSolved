4d5e2f9b4a4cca2b04572e42d74b7e209ffb0e17
Marcus Kim <marcuskim1989@gmail.com>
2023-07-19 18:56:58 -0700

    Problem 2283. Had trouble with this one again. Had to look up the answer. Will attempt again.

--- LeetCodeSolved.playground/Sources/2283. Check if Number Has Equal Digit Count and Digit Value .swift
+++ LeetCodeSolved.playground/Sources/2283. Check if Number Has Equal Digit Count and Digit Value .swift
@@ -1,5 +1,3 @@
-import Foundation
-
 class Solution2283 {
     func digitCount(_ num: String) -> Bool {
         
@@ -15,35 +13,62 @@ class Solution2283 {
 
         //return true
 
-        var arrayNum = Array(num)
+        
 
+        // var arrayNum = Array(num)
 
-        var dict: [Int: Int] = [:]
+        // if arrayNum.count == 1 {
+        //     if arrayNum[0].wholeNumberValue! != 0 {
+        //         return false
+        //     }
+        // }
 
-        for charDigit in arrayNum {
-            var digit = charDigit.wholeNumberValue!
+        // var dict: [Int: [Int]] = [:]
 
-            if dict[digit] != nil {
-                dict[digit]! += 1
-            } else {
-                dict[digit] = 1
-            }
-        }
+        // for charDigit in arrayNum {
+        //     var digit = charDigit.wholeNumberValue!
 
-        print(dict)
+        //     if dict[digit] != nil {
+        //         dict[digit]!.append(digit)
+        //     } else {
+        //         dict[digit] = [digit]
+        //     }
+        // }
 
-        for (index, charDigit) in arrayNum.enumerated() {
-            var digit = charDigit.wholeNumberValue!
+        // print(dict)
 
-            if index != dict[digit]{
-                return false
-            }
+        // for (index, charDigit) in arrayNum.enumerated() {
+        //     var digit = charDigit.wholeNumberValue!
 
-        }
+        //     print(index)
+
+        //     if dict[index] == nil {
+        //         continue
+        //     }
+
+        //     if digit != dict[index]!.count {
+        //         return false
+        //     }
+
+        // }
 
         
+        // return true
+
+        for (index, value) in num.enumerated() {
+            var check = 0
+
+            for i in num {
+                if String(i) == String(index) {
+                    check += 1
+                }
+            }
+
+            if String(check) != String(value) {
+                return false
+            }
+        }
+
         return true
-
-
     }
 }
