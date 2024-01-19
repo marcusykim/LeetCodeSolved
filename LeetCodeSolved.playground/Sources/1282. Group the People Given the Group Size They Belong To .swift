af00e1c106cad1c77302ab42ae39fe2052719667
Marcus Kim <marcuskim1989@gmail.com>
2024-01-18 22:36:27 -0800

    Problem 1282. Revisited. Didn't solve. Pasted the answer here. Need to try again in the future.

--- LeetCodeSolved.playground/Sources/1282. Group the People Given the Group Size They Belong To .swift
+++ LeetCodeSolved.playground/Sources/1282. Group the People Given the Group Size They Belong To .swift
@@ -1,39 +1,30 @@
 import Foundation
 
-class Solution1282 {
-    func groupThePeople(_ groupSizes: [Int]) -> [[Int]] {
+class Solution {
+func groupThePeople(_ groupSizes: [Int]) -> [[Int]] {
+    var ans = [[Int]]()
+    var groupDict = [Int: [Int]]()
+    
+    // Group people based on their group sizes.
+    for (i, groupSize) in groupSizes.enumerated() {
+        groupDict[groupSize, default: []].append(i)
+    }
+    
+    // Create groups of the specified sizes.
+    for (groupSize, groupMembers) in groupDict {
+        var group = [Int]()
         
-        
-        // declare dictionary Int: [Int]
-        // loop through the array
-        // for each element, add that element to a dictionary with the element as the key
-        // add to each key until key == array.count
-        // when key == array.count, add that array of values to result array
-        // set value in dictionary to empty array
-
-        var dict: [Int: [Int]] = [:]
-        var result: [[Int]] = []
-
-        for (index, num) in groupSizes.enumerated() {
+        // Split the group members into subgroups of the specified size.
+        for (index, member) in groupMembers.enumerated() {
+            group.append(member)
             
-            if dict[num] != nil {
-
-                if dict[num]!.count < num {
-                    dict[num]!.append(index)
-                    print(dict, "appending to existing key")
-                } else {
-                    print(index, "before appending to result")
-                    print(dict, "right before adding to result")
-                    result.append(dict[num]!)
-                    dict[num] = [index]
-                }
-            } else {
-                dict[num] = [index]
-                print(dict, "assign first value in empty key")
+            if (index + 1) % groupSize == 0 {
+                ans.append(group)
+                group = []
             }
         }
-
-        return result
-
     }
+    
+    return ans
+}
 }
