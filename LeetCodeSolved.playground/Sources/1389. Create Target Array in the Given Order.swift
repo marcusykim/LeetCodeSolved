ca09ffce83532cabbbe593ec0fda5bcfb38e9ee5
Marcus Kim <marcuskim1989@gmail.com>
2024-05-21 22:46:47 -0700

    Problem 2974. Remove, remove. Append in reverse order of removal

--- LeetCodeSolved.playground/Sources/1389. Create Target Array in the Given Order.swift
+++ LeetCodeSolved.playground/Sources/1389. Create Target Array in the Given Order.swift
@@ -1,2 +1,18 @@
 import Foundation
 
+class Solution {
+    func createTargetArray(_ nums: [Int], _ index: [Int]) -> [Int] {
+        
+        var targetArray: [Int] = []
+
+        // loop
+          // take the value at num[i] and insert it into targetArray at index[i]
+
+        for i in 0..<nums.count {
+            targetArray.insert(nums[i], at: index[i])
+        }
+
+        return targetArray
+
+    }
+}
--- LeetCodeSolved.playground/Sources/2974. Minimum Number Game.swift
+++ LeetCodeSolved.playground/Sources/2974. Minimum Number Game.swift
@@ -1,2 +1,31 @@
 import Foundation
 
+class Solution {
+    func numberGame(_ nums: [Int]) -> [Int] {
+        
+        // loop
+            // remove remove
+            // append in opposite order
+
+        var nums = nums
+
+        var result: [Int] = []
+
+        while nums.count > 0 {
+            var aliceMinRemoval = nums.min()
+            nums.remove(at: nums.index(of: aliceMinRemoval!)!)
+            var bobMinRemoval = nums.min()
+            nums.remove(at: nums.index(of: bobMinRemoval!)!)
+
+            result.append(bobMinRemoval!)
+            result.append(aliceMinRemoval!)
+
+        }
+
+
+
+
+        return result
+
+    }
+}
