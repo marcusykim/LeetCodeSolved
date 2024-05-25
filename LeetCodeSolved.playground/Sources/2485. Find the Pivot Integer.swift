202ebd680632394e559dc4d02f3d34fbc38150a7
Marcus Kim <marcuskim1989@gmail.com>
2024-05-24 22:45:14 -0700

    Problem 2485. Finally looked up answer. It requires that you know the mathematical solution. Never would've gotten it

--- LeetCodeSolved.playground/Sources/2485. Find the Pivot Integer.swift
+++ LeetCodeSolved.playground/Sources/2485. Find the Pivot Integer.swift
@@ -1,22 +1,7 @@
-import Foundation
-
 class Solution {
     func pivotInteger(_ n: Int) -> Int {
-        
-        // find mean value
-
-        // loop
-            // continue adding to result until mean value is met
-            // if none, return -1
-
-        var result = 0
-        var nArray = Array(1...n)
-        nArray.insert(nArray.count / 2, at: nArray.index(of: nArray.count ))
-        var mean = vDSP.mean(nArray)
-        nArray.insert(mean, at: nArray.index(of: mean))
-
-        var
-        
-
+        let y = (n * n + n) / 2
+        let x = Int(sqrt(Double(y)))
+        return x * x == y ? x : -1
     }
 }
