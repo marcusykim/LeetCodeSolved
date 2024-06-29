747370d1ef63a9361bc651f7a6025db3efaec26b
Marcus Kim <marcuskim1989@gmail.com>
2024-06-28 23:28:55 -0700

    Problem 2169. Loop. Use if condition to check which number is greater… … and subtract the other number from it, then increment the result. At the end of the loop, check if either reached 0. If so, return result

--- LeetCodeSolved.playground/Sources/2169. Count Operations to Obtain Zero.swift
+++ LeetCodeSolved.playground/Sources/2169. Count Operations to Obtain Zero.swift
@@ -1,2 +1,30 @@
 import Foundation
 
+class Solution {
+    func countOperations(_ num1: Int, _ num2: Int) -> Int {
+        
+        var num1 = num1
+        var num2 = num2
+
+            var result = 0
+
+            if num1 == 0 || num2 == 0 {
+                    return result
+                }
+
+            while true {
+                if num1 >= num2 {
+                    num1 -= num2
+                    result += 1
+                } else {
+                    num2 -= num1
+                    result += 1
+                }
+
+                if num1 == 0 || num2 == 0 {
+                    return result
+                }
+            }
+
+    }
+}
--- LeetCodeSolved.playground/Sources/2843. Count Symmetric Integers.swift
+++ LeetCodeSolved.playground/Sources/2843. Count Symmetric Integers.swift
@@ -1,2 +1,25 @@
 import Foundation
 
+class Solution {
+    func countSymmetricIntegers(_ low: Int, _ high: Int) -> Int {
+    var sum = 0
+    
+    for i in low...high {
+        let str = "\(i)"
+        if str.count % 2 == 0 {
+            let middle = str.count / 2
+            var half1 = 0
+            var half2 = 0
+            for num in str.prefix(middle) {
+                half1 += Int("\(num)")!
+            }
+            for num in str.suffix(middle) {
+                half2 += Int("\(num)")!
+            }
+            sum += half1 == half2 ? 1 : 0
+        }
+    }
+    
+    return sum
+}
+}
