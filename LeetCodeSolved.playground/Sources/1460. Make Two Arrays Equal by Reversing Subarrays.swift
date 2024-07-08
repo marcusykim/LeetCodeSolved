3a8c0f1fb9e1d872387725282828385f5b40bad6
Marcus Kim <marcuskim1989@gmail.com>
2024-07-07 18:01:46 -0700

    Problem 2124. Check if all "a" or all "b". Return true if either is true. Then, check whether first element is "b", return false if true. Then, loop. In each iteration, use compound "and" statement to check whether previous element is "b" while current element is "a". Return false if true. At the end, return true

--- LeetCodeSolved.playground/Sources/1460. Make Two Arrays Equal by Reversing Subarrays.swift
+++ LeetCodeSolved.playground/Sources/1460. Make Two Arrays Equal by Reversing Subarrays.swift
@@ -1,2 +1,18 @@
 import Foundation
 
+class Solution {
+    func canBeEqual(_ target: [Int], _ arr: [Int]) -> Bool {
+        
+       var arr = arr.sorted()
+       var target = target.sorted()
+
+       for i in 0..<target.count {
+            if target[i] != arr[i] {
+                return false
+            }
+       }
+
+       return true
+
+    }
+}
--- LeetCodeSolved.playground/Sources/2124. Check if All A's Appears Before All B's.swift
+++ LeetCodeSolved.playground/Sources/2124. Check if All A's Appears Before All B's.swift
@@ -1,2 +1,29 @@
 import Foundation
 
+class Solution {
+    func checkString(_ s: String) -> Bool {
+        
+        var sArray = Array(s)
+
+        if !sArray.contains("a") {
+            return true
+        } else if !sArray.contains("b") {
+            return true
+        }
+
+        var checkChar = sArray.first
+
+        if checkChar == "b" {
+            return false
+        }
+
+        for i in 1..<sArray.count {
+            if sArray[i - 1] == "b" && sArray[i] == "a" {
+                return false
+            }
+        }
+
+        return true
+
+    }
+}
