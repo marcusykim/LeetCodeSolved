97543192a13067b6f6f289b7b35987dd3af234a1
Marcus Kim <marcuskim1989@gmail.com>
2024-09-08 17:50:51 -0700

    Problem 3028. Define var boundary = 0. Loop. With each iteration, add next element in array. Whenever boundary == 0, increment result.

--- LeetCodeSolved.playground/Sources/3028. Ant on the Boundary.swift
+++ LeetCodeSolved.playground/Sources/3028. Ant on the Boundary.swift
@@ -1,2 +1,21 @@
 import Foundation
 
+class Solution {
+    func returnToBoundaryCount(_ nums: [Int]) -> Int {
+        
+        var boundary = 0
+        var result = 0
+
+
+        for steps in nums {
+            boundary += steps
+            if boundary == 0 {
+                result += 1
+            }
+        }
+
+
+        return result
+
+    }
+}
--- LeetCodeSolved.playground/Sources/3063. Linked List Frequency.swift
+++ LeetCodeSolved.playground/Sources/3063. Linked List Frequency.swift
@@ -1,2 +1,58 @@
 import Foundation
 
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+class Solution {
+    func frequenciesOfElements(_ head: ListNode?) -> ListNode? {
+        
+        // traverse linked list
+        // for each node, add value to dictionary, which also tracks frequency
+
+        // after traversing the whole list, iterate through dictionary, add the frequencies to a new list
+
+        var currentNode = head
+        var dict: [Int: Int] = [:]
+
+        while let node = currentNode {
+
+            if dict[node.val] == nil {
+                dict[node.val] = 1
+            } else {
+                dict[node.val]! += 1
+            }
+
+            currentNode = node.next
+        }
+
+        print(dict)
+
+        var resultListHead: ListNode?
+        var dictCurrentNode: ListNode?
+
+        for entry in dict {
+
+            print("pre if statement ", entry.value)
+            if resultListHead == nil {
+                resultListHead = ListNode(entry.value)
+                dictCurrentNode = resultListHead
+                print("inside if ", entry.value)
+            } else {
+                dictCurrentNode!.next = ListNode(entry.value)
+                dictCurrentNode = dictCurrentNode!.next
+                print("inside else ", entry.value)
+            }
+
+        }
+
+        return resultListHead
+
+    }
+}
