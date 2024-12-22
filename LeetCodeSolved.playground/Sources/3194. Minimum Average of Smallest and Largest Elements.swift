b0b0cfffc47d4e2b455db1fdc3b6f21d9186dabf
Marcus Kim <marcuskim1989@gmail.com>
2024-12-21 19:33:24 -0800

    Problem 3285. Loop, check previous element in loop

--- LeetCodeSolved.playground/Sources/3194. Minimum Average of Smallest and Largest Elements.swift
+++ LeetCodeSolved.playground/Sources/3194. Minimum Average of Smallest and Largest Elements.swift
@@ -1,2 +1,24 @@
 import Foundation
 
+class Solution {
+    func minimumAverage(_ nums: [Int]) -> Double {
+        
+        var averages: [Double] = []
+
+        var nums = nums.sorted()
+
+        for _ in 0..<nums.count / 2 {
+
+            var minElement = Double(nums.first!)
+            var maxElement = Double(nums.last!)
+            averages.append((minElement + maxElement) / 2.0)
+
+            nums.removeFirst()
+            nums.removeLast()
+
+        }
+
+        return averages.min()!
+
+    }
+}
---
+++ LeetCodeSolved.playground/Sources/3285. Find Indices of Stable Mountains'.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+class Solution {
+    func stableMountains(_ height: [Int], _ threshold: Int) -> [Int] {
+        
+        var result: [Int] = []
+
+        for i in 1..<height.count {
+            if height[i - 1] > threshold {
+                result.append(i)
+            }
+        }
+
+        return result
+
+    }
+}
+
