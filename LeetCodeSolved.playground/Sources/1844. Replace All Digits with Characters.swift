c59c81f93cba69c942be6f22d567a11e6a0cc056
Marcus Kim <marcuskim1989@gmail.com>
2025-01-20 23:58:06 -0800

    gjfkg

---
+++ LeetCodeSolved.playground/Sources/1844. Replace All Digits with Characters.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+class Solution {
+    func replaceDigits(_ s: String) -> String {
+        // need array that is the alphabet
+        // remember a is 1, not index zero
+        //loop, keeping track of s[i - 1] s[i]
+
+        var sArray = Array(s)
+        let alphabet = Array("abcdefghijklmnopqrstuvwxyz")
+
+
+        for i in stride(from: 0, to: s.count - 1, by: 2) {
+            var letter = sArray[i]
+            var num = sArray[i + 1].wholeNumberValue!
+
+            sArray[i + 1] = alphabet[alphabet.firstIndex(of: letter)! + num]
+        
+        }
+        print(sArray)
+
+        let result = sArray.joined(separator: "")
+
+        return ""
+
+    }
+}
