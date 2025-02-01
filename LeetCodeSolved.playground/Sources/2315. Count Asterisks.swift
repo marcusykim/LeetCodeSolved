936e7d28fdfdd05e8e2518aa25ffdc0de33fddab
Marcus Kim <marcuskim1989@gmail.com>
2025-01-31 23:10:15 -0800

    Problem 2315. Use Bool to track whether we're inside a pair

--- LeetCodeSolved.playground/Sources/2315. Count Asterisks.swift
+++ LeetCodeSolved.playground/Sources/2315. Count Asterisks.swift
@@ -1,45 +1,33 @@
 import Foundation
 
-class Solution2315 {
+class Solution {
     func countAsterisks(_ s: String) -> Int {
-        // loop through string until we find a |
-        // keep track of all the asterisks until we find the second |
-        // when we the find the next |
-        // add all the * seen so far into the result
+      
+        // loop
+            // increment counter until we reach our first |
+            // keep iterating, but don't count
+            // when we reach the second |
+                // keep iterating but count
 
-        var arrayS: [Character] = Array(s)
+        var sArray = Array(s)
+        var doWeCount = true
+        var result = 0
 
-        var foundFirst: Bool = false
-        var foundSecond: Bool = false
-
-        var tempCounter: Int = 0
-        var result: Int = 0
-
-        for char in arrayS {
-            
-            print("char: ", char)
-
-            if char == "|" && foundFirst == false {
-                print("inside if")
-                foundFirst = true
-            } else if char == "|" && foundFirst == true && foundSecond == false {
-                print("inside first else if")
-                foundSecond = true
-            } else if char == "|" && foundFirst == true && foundSecond == true {
-                print("inside second else if")
-                result += tempCounter
-                tempCounter = 0
-                foundFirst = false
-                foundSecond = false
-            } else if char == "*" && foundFirst == false && foundSecond == false {
-                print("inside third else if")
-                tempCounter += 1
-            } else if char == "*" && foundFirst == true && foundSecond == true {
-                print("inside fourth else if")
-                tempCounter += 1
+        for char in sArray {
+            if char == "|" && doWeCount == true {
+                doWeCount = false
+            } else if char == "|" && doWeCount == false {
+                doWeCount = true
             }
-        }
+            
+            if doWeCount == true {
+                if char == "*" {
+                    result += 1
+                }
+            }
 
+
+        }
 
         return result
     }
