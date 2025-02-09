473e39750b5618bb23109b5dda2c9ff0ca24e132
Marcus Kim <marcuskim1989@gmail.com>
2025-02-08 22:39:19 -0800

    Problem 3340. Iterate through num, splitting even and odd indices into two separate arrays. Sum the arrays. Compare

--- LeetCodeSolved.playground/Sources/3340. Check Balanced String.swift
+++ LeetCodeSolved.playground/Sources/3340. Check Balanced String.swift
@@ -1,2 +1,32 @@
 import Foundation
 
+class Solution {
+    func isBalanced(_ num: String) -> Bool {
+        
+        var numArray = Array(num)
+        var oddNumArray: [Character] = []
+        var evenNumArray: [Character] = []
+
+        for (index, num) in numArray.enumerated() {
+            if index % 2 != 0 {
+                oddNumArray.append(num)
+            } else {
+                evenNumArray.append(num)
+            }
+        }
+
+        var oddIndexSum = 0
+        var evenIndexSum = 0
+
+        for num in oddNumArray {
+            oddIndexSum += num.wholeNumberValue!
+        }
+
+        for num in evenNumArray {
+            evenIndexSum += num.wholeNumberValue!
+        }
+
+        return evenIndexSum == oddIndexSum
+
+    }
+}
